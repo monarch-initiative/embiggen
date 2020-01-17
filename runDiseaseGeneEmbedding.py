ba81beb759bbb8298112aa35bb855ed5c9863181
Justin Reese <justaddcoffee+github@gmail.com>
2020-01-17 13:38:01 -0800

    Add click() to runDiseaseGeneEmbedding.py for tidier command line processing

--- runDiseaseGeneEmbedding.py
+++ runDiseaseGeneEmbedding.py
@@ -1,49 +1,49 @@
-
-
-# 1 Input test graph
-#from gensim.models import Word2Vec
+import click
 import n2v
 from n2v import CSFGraph
-import os
-
 from n2v.word2vec import SkipGramWord2Vec
 
-training_file = os.path.join(os.path.dirname(__file__), 'tests', 'data', 'karate.train')
-training_file = '/home/robinp/PycharmProjects/IDG2KG-project-management/n2v/edgelist.txt'
-#test_file= os.path.join(os.path.dirname(__file__), 'tests', 'data', 'karate.test')
+@click.group()
+def cli():
+    pass
 
-print("Reading training file %s" % training_file)
-training_graph = CSFGraph(training_file)
-print(training_graph)
-training_graph.print_edge_type_distribution()
+@cli.command()
+@click.option("training_file", "-t", type=click.Path(exists=True))
+@click.option("output_file", "-o", default='disease.embedded')
+@click.option("p", "-p", type=int, default=1)
+@click.option("q", "-q", type=int, default=1)
+@click.option("gamma", "-g", type=int, default=1)
+@click.option("use_gamma", "-u", is_flag=True, default=False)
+@click.option("walk_length", "-w", type=int, default=80)
+@click.option("num_walks", "-n", type=int, default=25)
+@click.option("dimensions", "-d", type=int, default=128)
+@click.option("window_size", "-w", type=int, default=10)
+@click.option("workers", "-r", type=int, default=8)
+@click.option("num_steps", "-s", type=int, default=100000)
+@click.option("display_step", "-d", type=int, default=1000)
+def disease_gene_embeddings(training_file, output_file, p, q, gamma, use_gamma,
+                            walk_length, num_walks, dimensions, window_size, workers,
+                            num_steps, display_step):
+    print("Reading training file %s" % training_file)
+    training_graph = CSFGraph(training_file)
+    print(training_graph)
+    training_graph.print_edge_type_distribution()
 
+    hetgraph = n2v.hetnode2vec.N2vGraph(training_graph, p, q, gamma, use_gamma)
+    walks = hetgraph.simulate_walks(num_walks, walk_length)
+    worddictionary = training_graph.get_node_to_index_map()
+    reverse_worddictionary = training_graph.get_index_to_node_map()
 
-p = 1
-q = 1
-gamma = 1
-useGamma = False
-hetgraph = n2v.hetnode2vec.N2vGraph(training_graph, p, q, gamma, useGamma)
+    numberwalks = []
+    for w in walks:
+        nwalk = []
+        for node in w:
+            i = worddictionary[node]
+            nwalk.append(i)
+        numberwalks.append(nwalk)
 
-walk_length = 80
-num_walks = 25
-walks = hetgraph.simulate_walks(num_walks, walk_length)
-#walks = [map(str, walk) for walk in walks]
-dimensions = 128
-window_size = 10
-workers = 8
-
-worddictionary = training_graph.get_node_to_index_map()
-reverse_worddictionary = training_graph.get_index_to_node_map()
-
-numberwalks = []
-for w in walks:
-    nwalk = []
-    for node in w:
-        i = worddictionary[node]
-        nwalk.append(i)
-    numberwalks.append(nwalk)
-
-model = SkipGramWord2Vec(numberwalks, worddictionary=worddictionary, reverse_worddictionary=reverse_worddictionary, num_steps=100000)
-model.train(display_step=1000)
-output_filenname = 'disease.embedded'
-model.write_embeddings(output_filenname)
+    model = SkipGramWord2Vec(numberwalks, worddictionary=worddictionary,
+                             reverse_worddictionary=reverse_worddictionary,
+                             num_steps=num_steps)
+    model.train(display_step=display_step)
+    model.write_embeddings(output_file)
