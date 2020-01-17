9684f77acb4cf641d2654f345049dea9be9e3186
Justin Reese <justaddcoffee+github@gmail.com>
2020-01-17 13:48:35 -0800

    Add disease link command

--- runDiseaseGeneEmbedding.py
+++ runDiseaseGeneEmbedding.py
@@ -1,7 +1,10 @@
+import logging
+
 import click
 import n2v
 from n2v import CSFGraph
 from n2v.word2vec import SkipGramWord2Vec
+from n2v import LinkPrediction
 
 @click.group()
 def cli():
@@ -24,6 +27,10 @@ def cli():
 def disease_gene_embeddings(training_file, output_file, p, q, gamma, use_gamma,
                             walk_length, num_walks, dimensions, window_size, workers,
                             num_steps, display_step):
+    """
+    Generate disease gene embeddings
+    """
+    logging.basicConfig(level=logging.INFO)
     print("Reading training file %s" % training_file)
     training_graph = CSFGraph(training_file)
     print(training_graph)
@@ -47,3 +54,25 @@ def disease_gene_embeddings(training_file, output_file, p, q, gamma, use_gamma,
                              num_steps=num_steps)
     model.train(display_step=display_step)
     model.write_embeddings(output_file)
+
+@cli.command()
+@click.option("test_file", "-t", type=click.Path(exists=True))
+@click.option("training_file", "-r", type=click.Path(exists=True))
+@click.option("embedded_graph", "-e", type=click.Path(exists=True))
+def disease_link_prediction(test_file, training_file, embedded_graph):
+    """
+    Predict disease links
+    """
+    test_graph = CSFGraph(test_file)
+    training_graph = CSFGraph(training_file)
+
+    parameters = {'edge_embedding_method': "hadamard",
+                  'portion_false_edges': 1}
+
+    lp = LinkPrediction(training_graph, test_graph, embedded_graph, params=parameters)
+    lp.predict_links()
+    lp.output_Logistic_Reg_results()
+    training_graph = CSFGraph(training_file)
+
+if __name__ == "__main__":
+    cli()
