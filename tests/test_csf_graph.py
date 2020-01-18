1cbe7d93502af6e64ac8b1ec7adbbe524aeb0215
Luca Cappelletti <cappelletti.luca94@gmail.com>
2020-01-18 23:04:11 +0100

    Extended testing coverage (72%)

--- tests/test_csf_graph.py
+++ tests/test_csf_graph.py
@@ -5,12 +5,23 @@ from n2v import CSFGraph
 
 class TestCSFGraph(TestCase):
     def setUp(self):
-        inputfile = os.path.join(os.path.dirname(__file__), 'data', 'small_graph.txt')
+        inputfile = os.path.join(os.path.dirname(
+            __file__), 'data', 'small_graph.txt')
         g = CSFGraph(inputfile)
         self.g = g
+        str(g)
 
     def test_notnull(self):
         self.assertIsNotNone(self.g)
+
+    def test_get_node_to_index_map(self):
+        self.g.get_node_to_index_map()
+
+    def test_get_index_to_node_map(self):
+        self.g.get_index_to_node_map()
+
+    def test_print_edge_type_distribution(self):
+        self.g.print_edge_type_distribution()
 
     def test_count_nodes(self):
         # The graphh in small_graph.txt has 11 nodes
@@ -25,7 +36,7 @@ class TestCSFGraph(TestCase):
         self.assertEqual(expected_num, self.g.edge_count())
 
     def test_get_weight(self):
-        expected = 10 # for d1<->d3
+        expected = 10  # for d1<->d3
         weight = self.g.weight('d1', 'd3')
         self.assertEqual(expected, weight)
         expected = 7  # for g3<->g4
@@ -35,13 +46,16 @@ class TestCSFGraph(TestCase):
     def test_get_neighbors1(self):
         # The neighhbors of p4 are g4, p2, p3
         nbrs = self.g.neighbors('p4')
-        #print(nbrs)
-        self.assertEqual(['g4','p2','p3'], nbrs)
+        # print(nbrs)
+        self.assertEqual(['g4', 'p2', 'p3'], nbrs)
+
+    def test_nodes_as_integers(self):
+        self.g.nodes_as_integers()
 
     def test_get_neighbors2(self):
         # The neighbors of g2 are g1, g3, p1, p2
         nbrs = self.g.neighbors('g2')
-        #print(nbrs)
+        # print(nbrs)
         self.assertEqual(['g1', 'g3', 'p1', 'p2'], nbrs)
 
     def test_check_nodetype(self):
@@ -63,4 +77,3 @@ class TestCSFGraph(TestCase):
         self.assertTrue(t2 in edge_list)
         made_up = ('z1', 'q123')
         self.assertFalse(made_up in edge_list)
-
