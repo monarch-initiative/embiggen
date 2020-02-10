993d7a79e093a9724c1719446bd8715afe21ccf6
callahantiff <callahantiff@gmail.com>
2020-02-09 18:25:43 -0700

    light refactoring of test methods

--- tests/test_word2vec.py
+++ tests/test_word2vec.py
@@ -10,41 +10,48 @@ import os.path
 
 class TestCBOWconstruction(TestCase):
     """
-    Use the test files in test/data/ppismall to construct a CBOW and test that we can get some random walks
+    Use the test files in test/data/ppismall to construct a CBOW and test that we can get some random walks.
     """
     def setUp(self):
         # neg_test = 'data/ppismall/neg_test_edges'
         # neg_train = 'data/ppismall/neg_train_edges'
-        #pos_test = 'data/ppismall/pos_test_edges'
+        # pos_test = 'data/ppismall/pos_test_edges'
         curdir = os.path.dirname(__file__)
         pos_train = os.path.join(curdir, "data/ppismall/pos_train_edges")
         pos_train = os.path.abspath(pos_train)
+
         training_graph = CSFGraph(pos_train)
         worddictionary = training_graph.get_node_to_index_map()
         reverse_worddictionary = training_graph.get_index_to_node_map()
-        p = 1
-        q = 1
-        gamma = 1
-        useGamma = False
+        p, q, gamma = 1, 1, 1
+        use_gamma = False
         self.number_of_nodes_in_training = training_graph.node_count()
-        self.n2v_graph = N2vGraph(training_graph, p, q, gamma, useGamma)
+        self.n2v_graph = N2vGraph(training_graph, p, q, gamma, use_gamma)
+
+        # generate walks
         self.walk_length = 10
         self.num_walks = 5
         walks = self.n2v_graph.simulate_walks(self.num_walks, self.walk_length)
+
         walks_integer_nodes = []
         for w in walks:
             nwalk = []
+
             for node in w:
                 i = worddictionary[node]
                 nwalk.append(i)
+
             walks_integer_nodes.append(nwalk)
+
         self.walks = walks_integer_nodes
-        self.cbow = ContinuousBagOfWordsWord2Vec(self.walks, worddictionary=worddictionary,
-                               reverse_worddictionary=reverse_worddictionary, num_steps=100)
+        self.cbow = ContinuousBagOfWordsWord2Vec(self.walks,
+                                                 worddictionary=worddictionary,
+                                                 reverse_worddictionary=reverse_worddictionary,
+                                                 num_steps=100)
 
     def test_number_of_walks_performed(self):
 
-        # Expected number of walks is num_walks times the number of nodes in the training graph
+        # expected number of walks is num_walks times the number of nodes in the training graph
         expected_n_walks = self.num_walks * self.number_of_nodes_in_training
         self.assertEqual(expected_n_walks, len(self.walks))
 
