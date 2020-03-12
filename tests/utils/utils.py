94a21f3435b302fd1492cef25937ced7e402444d
callahantiff <callahantiff@gmail.com>
2020-03-12 12:02:31 -0600

    added tf functions
    Added functions to return the length of a tf object and search for an edge in a tf object

--- tests/utils/utils.py
+++ tests/utils/utils.py
@@ -1,4 +1,7 @@
 import numpy as np  # type:ignore
+import tensorflow as tf  # type:ignore
+
+from typing import List, Tuple, Union
 
 
 def calculate_total_probs(j: np.ndarray, q: np.ndarray) -> np.array:
@@ -27,3 +30,45 @@ def calculate_total_probs(j: np.ndarray, q: np.ndarray) -> np.array:
     s = np.sum(probs)
 
     return probs / s
+
+
+def gets_tensor_length(tensor: Union[tf.data.Dataset, tf.RaggedTensor]) -> int:
+    """Returns the length of a tensorflow object.
+
+    Args:
+        tensor: A tf.Dataset or tf.RaggedTensor object.
+
+    Returns:
+        An integer representing the length of the tensor.
+    """
+
+    if isinstance(tensor, tf.RaggedTensor):
+        return tensor.shape[0]
+    else:
+        return len([edge for edge in tensor])
+
+
+def searches_tensor(test_edge:  Union[Tuple, List], tensor: Union[tf.data.Dataset, tf.RaggedTensor]) -> bool:
+    """Searches a tensorflow object for an edge, which is represented as a tensorflow Variable object.
+
+    NOTE. This method works for tf.Dataset and tf.RaggedTensor objects.
+
+    Args:
+        test_edge: A tf.Variable object.
+        tensor: A tf.Dataset or tf.RaggedTensor object.
+
+    Returns:
+        An bool indicating whether or not the test_edge was found in the tensor flow object.
+    """
+
+    # check if edge object contains ints or string
+    if all(x for x in test_edge if isinstance(x, str)):
+        tf_edge = tf.Variable(test_edge, dtype=tf.string)
+    else:
+        tf_edge = tf.Variable(test_edge, dtype=tf.int32)
+
+    # find edge in tensor stack
+    search_result = [edge for edge in tensor if all(node for node in (edge == tf_edge).numpy())]
+
+    # if edge is in tensor stack, length should be greater than 1
+    return len(search_result) > 0
