45fe4a7af57de6cf3abe8df9ee4edd7ad40a8789
Justin Reese <justaddcoffee+github@gmail.com>
2020-03-12 11:36:36 -0700

    Fix failing test in test_word2vec.py

--- tests/test_word2vec.py
+++ tests/test_word2vec.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-
+import os
 from xn2v import CBOWBatcherListOfLists
 from xn2v import CSFGraph
 from xn2v import N2vGraph
@@ -71,7 +71,9 @@ class TestCBOWconstruction(TestCase):
         # neg_test = 'data/ppismall/neg_test_edges'
         # neg_train = 'data/ppismall/neg_train_edges'
         #pos_test = 'data/ppismall/pos_test_edges'
-        pos_train = 'data/ppismall/pos_train_edges'
+        curdir = os.path.dirname(__file__)
+        pos_train = os.path.join(curdir, "data/ppismall/pos_train_edges")
+        pos_train = os.path.abspath(pos_train)
         training_graph = CSFGraph(pos_train)
         worddictionary = training_graph.get_node_to_index_map()
         reverse_worddictionary = training_graph.get_index_to_node_map()
