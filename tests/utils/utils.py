cf570320c23fb04c7c878d5ea8aafff010d7ee20
callahantiff <callahantiff@gmail.com>
2020-03-12 10:37:12 -0600

    added Google Style Guidelines and typing

--- tests/utils/utils.py
+++ tests/utils/utils.py
@@ -1,20 +1,29 @@
-import numpy as np
+import numpy as np  # type:ignore
 
 
-def calculate_total_probs(j, q):
+def calculate_total_probs(j: np.ndarray, q: np.ndarray) -> np.array:
+    """Use the alias method to calculate the total probabilities of the discrete events.
+
+    Args:
+        j: A vector of node aliases (e.g. array([2, 3, 0, 2])).
+        q: A vector of alias-method probabilities (e.g. array([0.5, 0.5, 1. , 0.5])).
+
+    Returns:
+        An array of floats representing the total probabilities of the discrete event (e.g. array([0.125, 0.125,
+        0.5  , 0.25 ])).
     """
-    Use the alias method to calculate the total probabilites of the discrete events
-    :param j: alias vector
-    :param q: alias-method probabilities
-    :return:
-    """
-    N = len(j)
-    probs = np.zeros(N)
-    for i in range(N):
+
+    n = len(j)
+    probs = np.zeros(n)
+
+    for i in range(n):
         p = q[i]
         probs[i] += p
+
         if p < 1.0:
             alias_index = j[i]
             probs[alias_index] += 1 - p
+
     s = np.sum(probs)
+
     return probs / s
