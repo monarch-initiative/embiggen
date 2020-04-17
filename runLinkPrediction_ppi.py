15dee1bf8aab47a8445a6b3d53e9b8071d363636
Vida Ravanmehr <vidarmehr@gmail.com>
2020-04-17 09:37:56 -0400

    read_embeddings() was missing!

--- runLinkPrediction_ppi.py
+++ runLinkPrediction_ppi.py
@@ -85,7 +85,7 @@ def parse_args():
     parser.add_argument('--w2v-model', nargs='?', default='Skipgram',
                         help="word2vec model. It can be either Skipgram or CBOW")
 
-    parser.add_argument('--num_steps', type=int, default=100000,
+    parser.add_argument('--num_steps', type=int, default=100,
                         help='number of steps for GD.  Default is 100000.')
 
     return parser.parse_args()
