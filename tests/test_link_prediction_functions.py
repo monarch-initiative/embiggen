602c6f3eaa8070a186cd7015ef81141c0bb89a01
Vida Ravanmehr <vidarmehr@gmail.com>
2020-04-22 12:03:03 -0400

    adding more tests to check link prediction scores

--- tests/test_link_prediction_functions.py
+++ tests/test_link_prediction_functions.py
@@ -5,7 +5,7 @@ from xn2v import link_prediction_functions
 import numpy as np
 
 
-class TestCSFGraph(TestCase):
+class TestlinkPredictionScores_1(TestCase):
     def setUp(self):
         inputfile = os.path.join(os.path.dirname(
             __file__), 'data', 'small_graph.txt')
@@ -94,3 +94,40 @@ class TestCSFGraph(TestCase):
         score = link_prediction_functions.AdamicAdar(self.g, node_1, node_2)
         self.assertAlmostEqual((1.0/np.log(3)) + (1.0/np.log(5)), score)
 
+class TestlinkPredictionScores_2(TestCase):
+    def setUp(self):
+        inputfile = os.path.join(os.path.dirname(
+            __file__), 'data', 'small_het_graph.txt')
+        g = CSFGraph(inputfile)
+        self.g = g
+        str(g)
+
+    def test_notnull(self):
+        self.assertIsNotNone(self.g)
+
+    def test_DegreeProduct_1(self):
+        ""
+        # Test the Degree product score of two nodes
+        node_1 = "g1"
+        node_2 = "d2"
+        # neighbors of "g1" = {g0,g2,g3,...,g100,p1,d1}, neighbors of "d2" = {d1}. Thus the score = 102 * 1 = 102
+        score = link_prediction_functions.DegreeProduct(self.g, node_1, node_2)
+        self.assertEqual(102, score)
+
+    def test_DegreeProduct_2(self):
+        ""
+        # Test the Degree product score of two nodes
+        node_1 = "g3"
+        node_2 = "p3"
+        # neighbors of "g3" = {g1}, neighbors of "p3" = {p1}. Thus the score = 1 * 1 = 1
+        score = link_prediction_functions.DegreeProduct(self.g, node_1, node_2)
+        self.assertEqual(1, score)
+
+    def test_DegreeProduct_3(self):
+        ""
+        # Test the Degree product score of two nodes
+        node_1 = "g3"
+        node_2 = "g4"
+        # neighbors of "g3" = {g1}, neighbors of "g4" = {g1}. Thus the score = 1 * 1 = 1
+        score = link_prediction_functions.DegreeProduct(self.g, node_1, node_2)
+        self.assertEqual(1, score)
