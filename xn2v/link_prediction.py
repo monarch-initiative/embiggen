78ec860ce51b6c54eba291815e84cf418fc0d65e
Justin Reese <justaddcoffee+github@gmail.com>
2020-05-01 12:26:01 -0700

    mypy fixes

--- xn2v/link_prediction.py
+++ xn2v/link_prediction.py
@@ -1,6 +1,6 @@
 import sys
 import numpy as np   # type: ignore
-from sklearn.calibration import CalibratedClassifierCV
+from sklearn.calibration import CalibratedClassifierCV  # type:ignore
 
 from sklearn.linear_model import LogisticRegression   # type: ignore
 from sklearn import metrics   # type: ignore
