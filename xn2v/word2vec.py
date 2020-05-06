85380a118a67444731fddc75c4c6402b00d3faec
Vida Ravanmehr <vidarmehr@gmail.com>
2020-05-06 11:50:00 -0400

    set n_epoch=2 for now to get fast result. It will be in args later.

--- xn2v/word2vec.py
+++ xn2v/word2vec.py
@@ -325,7 +325,7 @@ class SkipGramWord2Vec(Word2Vec):
 
         x_test = np.array(self.display_examples)
 
-        n_epochs = 25
+        n_epochs = 2
         window_len = 2 * self.skip_window + 1
         step = 0
         loss_history = []
@@ -663,7 +663,7 @@ class ContinuousBagOfWordsWord2Vec(Word2Vec):
             self.display_words()
         # n_epochs = 2
         # data = self.data  #
-        n_epochs = 25
+        n_epochs = 2
         window_len = 2 * self.skip_window + 1
         step = 0
         loss_history = []
