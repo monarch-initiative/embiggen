9fdb0a6c16bb6ee528794c3fce60737acd22dee4
Justin Reese <justaddcoffee+github@gmail.com>
2020-05-08 14:18:42 -0700

    Tests for LinkPrediction

--- tests/test_link_prediction.py
+++ tests/test_link_prediction.py
@@ -1,9 +1,47 @@
+import os
 import unittest
+from tempfile import gettempdir
+
+from parameterized import parameterized
+
+from runLinkPrediction_ppi import learn_embeddings
+from xn2v import LinkPrediction, CSFGraph, N2vGraph
 
 
 class TestLinkPrediction(unittest.TestCase):
-    def test_equalit(self):
-        self.assertEqual(1, 1)
+    def setUp(self) -> None:
+        self.file_dir = 'tests/data/ppismall_with_validation/'
+        self.pos_train_graph = CSFGraph(os.path.join(self.file_dir, 'pos_train_edges_max_comp_graph'))
+        self.pos_valid_graph = CSFGraph(os.path.join(self.file_dir, 'pos_validation_edges_max_comp_graph'))
+        self.pos_test_graph = CSFGraph(os.path.join(self.file_dir, 'pos_test_edges_max_comp_graph'))
+        self.neg_train_graph = CSFGraph(os.path.join(self.file_dir, 'neg_train_edges_max_comp_graph'))
+        self.neg_valid_graph = CSFGraph(os.path.join(self.file_dir, 'neg_validation_edges_max_comp_graph'))
+        self.neg_test_graph = CSFGraph(os.path.join(self.file_dir, 'neg_test_edges_max_comp_graph'))
+        self.test_embeddings = os.path.join(self.file_dir, 'test.embeddings')
+
+    @parameterized.expand([
+                            ("LR",),
+                            ("RF",),
+                            ("MLP",),
+                            ("FFNN",),
+                            ("MultiModalFFNN",),
+    ])
+    def test_classifiers(self, classifier_name):
+        lp = LinkPrediction(
+                            self.pos_train_graph,
+                            self.pos_valid_graph,
+                            self.pos_test_graph,
+                            self.neg_train_graph,
+                            self.neg_valid_graph,
+                            self.neg_test_graph,
+                            self.test_embeddings,
+                            'hadamard',
+                            classifier=classifier_name,
+                            use_valid=True)
+
+        lp.prepare_edge_and_node_labels()
+        lp.predict_links()
+        lp.output_classifier_results()
 
 
 if __name__ == '__main__':
