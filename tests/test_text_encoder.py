d3cc4052bc2a2144370fa91162799d425b85055a
callahantiff <callahantiff@gmail.com>
2020-05-25 11:37:06 -0600

    extended coverage to include input params

--- tests/test_text_encoder.py
+++ tests/test_text_encoder.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 from unittest import TestCase
 
-from embiggen.text_encoder import TextEncoder  # update once refactor is complete
+from embiggen.text_encoder import TextEncoder
 from tests.utils.utils import gets_tensor_length
 
 
@@ -15,11 +15,30 @@ class TestTextEncoderSentences(TestCase):
     def setUp(self):
 
         # read in data
-        infile = os.path.join(os.path.dirname(__file__), 'data', 'greatExpectations3.txt')
+        self.infile = os.path.join(os.path.dirname(__file__), 'data', 'greatExpectations3.txt')
 
         # run text encoder
-        encoder = TextEncoder(filename=infile, data_type='sentences')
+        encoder = TextEncoder(filename=self.infile,
+                              payload_index=None,
+                              header=None,
+                              delimiter=None,
+                              data_type='sentences')
+
         self.data, self.count, self.dictionary, self.reverse_dictionary = encoder.build_dataset()
+
+    def testClassInitializationDataType(self):
+        """Tests the initialization of the class for the data_type parameter."""
+
+        # when passing a word other than "words" or "sentences"
+        self.assertRaises(TypeError, TextEncoder, self.infile, 'line')
+
+    def testParseSuccess(self):
+
+        # check returned data types
+        self.assertIsInstance(self.data, tf.RaggedTensor)
+        self.assertIsInstance(self.count, list)
+        self.assertIsInstance(self.dictionary, dict)
+        self.assertIsInstance(self.reverse_dictionary, dict)
 
     def testWordCounts(self):
 
@@ -34,7 +53,7 @@ class TestTextEncoderSentences(TestCase):
         # stopwords should have been removed
         self.assertEqual(None, wordcount.get('the'))
 
-    def test_number_of_sentences(self):
+    def testNumberOfSentences(self):
 
         # test length of tensor
         tensor_length = gets_tensor_length(self.data)
@@ -50,8 +69,21 @@ class TestTextEncoderEnBlock(TestCase):
         infile = os.path.join(os.path.dirname(__file__), 'data', 'greatExpectations3.txt')
 
         # run text encoder
-        encoder = TextEncoder(filename=infile, data_type='words')
+        encoder = TextEncoder(filename=infile,
+                              payload_index=None,
+                              header=None,
+                              delimiter=None,
+                              data_type='words')
+
         self.data, self.count, self.dictionary, self.reverse_dictionary = encoder.build_dataset()
+
+    def testParseSuccess(self):
+
+        # check returned data types
+        self.assertIsInstance(self.data, tf.Tensor)
+        self.assertIsInstance(self.count, list)
+        self.assertIsInstance(self.dictionary, dict)
+        self.assertIsInstance(self.reverse_dictionary, dict)
 
     def testWordCounts(self):
 
@@ -66,7 +98,7 @@ class TestTextEncoderEnBlock(TestCase):
         self.assertEqual(1, wordcount['spiders'])
         self.assertEqual(2, wordcount['twig'])
         self.assertEqual(2, wordcount['blade'])
-        self.assertEqual(None, wordcount.get('the'))  # Stopwords should have been removed
+        self.assertEqual(None, wordcount.get('the'))  # stopwords should have been removed
 
 
 class TestCsvTextEncoder(TestCase):
@@ -76,20 +108,49 @@ class TestCsvTextEncoder(TestCase):
 
     def setUp(self):
         # read in data
-        infile = os.path.join(os.path.dirname(__file__), 'data', 'pubmed20n1015excerpt.txt')
+        self.infile2 = os.path.join(os.path.dirname(__file__), 'data', 'pubmed20n1015excerpt.txt')
 
         # run text encoder
-        encoder = TextEncoder(filename=infile, data_type='sentences')
-        self.data, self.count, self.dictionary, self.reverse_dictionary = encoder.build_dataset_from_csv(payload_index=2)
+        encoder = TextEncoder(filename=self.infile2,
+                              payload_index=2,
+                              header=None,
+                              delimiter='\t',
+                              data_type='sentences')
+
+        self.data, self.count, self.dictionary, self.reverse_dictionary = encoder.build_dataset()
+
+    def testClassInitializationPayloadIndex(self):
+        """Tests the initialization of the class for the payload_index parameter."""
+
+        # when passing a non-integer
+        self.assertRaises(TypeError, TextEncoder, self.infile2, 'two', None, None, 'words')
+
+    def testClassInitializationHeader(self):
+        """Tests the initialization of the class for the header parameter."""
+
+        # when passing a non-integer
+        self.assertRaises(TypeError, TextEncoder, self.infile2, 2, 'two', None, 'words')
+
+    def testClassInitializationDelimiter(self):
+        """Tests the initialization of the class for the delimiter parameter."""
+
+        # when passing something other than a string
+        self.assertRaises(TypeError, TextEncoder, self.infile2, 2, None, [','], 'words')
 
     def testParseSuccess(self):
-        self.assertIsNotNone(self.data)
+
+        # check returned data types
+        self.assertIsInstance(self.data, tf.RaggedTensor)
+        self.assertIsInstance(self.count, list)
+        self.assertIsInstance(self.dictionary, dict)
+        self.assertIsInstance(self.reverse_dictionary, dict)
 
     def testCsvParserIngestedTenLines(self):
+
         expected_number_of_rows = 10
         i = 0
-        # self.assertTrue(isinstance(self.data, tf.Tensor))
+
         for _ in self.data:
             i += 1
-        self.assertEqual(expected_number_of_rows, i)
 
+        self.assertEqual(expected_number_of_rows, i)
