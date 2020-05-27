3ace92a1baf6f34c6a4bb912e6e773caf7e59a8f
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-05-27 08:38:22 +0200

    Removed old test

--- tests/test_graph_cashe.py
+++ tests/test_graph_cashe.py
@@ -32,28 +32,6 @@ class TestGraphCache(TestCase):
         g.simulate_walks(num_walks, walk_length)
         assert len(g.random_walks_map) > 0
 
-    def test_caching_restore(self):
-        """
-        Test caching of random walks in N2vGraph by,
-            - creating the N2vGraph
-            - running simulate_walks and storing the walks as a property of N2vGraph
-            - restoring the exact same walk for the same num_walks and walk_length
-        """
-        p = 1
-        q = 1
-        gamma = 1
-        num_walks = 10
-        walk_length = 1
-        g = N2vGraph(self.graph, p, q)
-        walks1 = g.simulate_walks(num_walks, walk_length)
-        walks2 = g.simulate_walks(num_walks, walk_length, use_cache=True)
-        # Expected: walks2 should be retrieved from cache since use_cache is True
-        assert walks1 == walks2
-        walks3 = g.simulate_walks(num_walks, walk_length, use_cache=False)
-        # Expected: walks3 should be a newly randomized walk
-        # and should not match with walks1
-        assert walks1 != walks3
-
     def test_cache_graph_state(self):
         """
         Test caching of N2vGraph state into a pickle.
