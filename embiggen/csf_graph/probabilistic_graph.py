229097f3d495971445185d4744c687b827ddd139
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-05-29 12:17:07 +0200

    Extended docstrings

--- embiggen/csf_graph/probabilistic_graph.py
+++ embiggen/csf_graph/probabilistic_graph.py
@@ -1,13 +1,27 @@
-import multiprocessing as mp
+from multiprocessing import cpu_count, Pool
 from .graph import Graph
+from typing import Dict
 from .probabilistic_graph_utils import alias_setup, alias_draw
+from numba import njit
+
 
 class ProbabilisticGraph(Graph):
 
-    def __init__(self, workers:int = -1, **kwargs):
-        super().__init__(normalize_weights=True, **kwargs)
+    def __init__(self, workers: int = -1, **kwargs: Dict):
+        """Create the ProbabilisticGraph object.
 
-        with mp.Pool(mp.cpu_count()) as pool:
+        Parameters
+        ----------------------
+        workers: int = -1
+            Number of processes to use. Use a number lower or equal to 0 to
+            use all available processes. Default is -1.
+        **kwargs: Dict,
+            Parameters to pass to the parent Graph class.
+        """
+        super().__init__(normalize_weights=True, **kwargs)
+        workers = workers if workers <= 0 else cpu_count()
+
+        with Pool(min(workers, self._neighbours_weights)) as pool:
             self._neighbours_alias = pool.map(
                 alias_setup,
                 self._neighbours_weights
@@ -15,8 +29,9 @@ class ProbabilisticGraph(Graph):
             pool.close()
             pool.join()
 
-    def extract_random_neighbour(self, node_index : int) -> int:
-        """Extract a random adiacent node to the one associated to node_index.
+    @njit
+    def extract_random_neighbour(self, node_index: int) -> int:
+        """Return a random adiacent node to the one associated to node_index.
         The Random is extracted by using the normalized weights of the edges
         as probability distribution. 
 
@@ -24,6 +39,7 @@ class ProbabilisticGraph(Graph):
         ----------
         node_index:int
             The index of the node that is to be considered.
+
         Returns
         -------
         The index of a random adiacent node to node_index.
