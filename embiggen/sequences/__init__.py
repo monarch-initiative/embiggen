de32a62479c17c3efc63e7f7acf78038bd9133b6
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-07-16 15:57:41 +0200

    Solved some pylint errors

--- embiggen/sequences/__init__.py
+++ embiggen/sequences/__init__.py
@@ -1,3 +1,4 @@
+"""Module with Keras Sequences."""
 from .node2vec_sequence import Node2VecSequence
 from .node_binary_skipgram_sequence import NodeBinarySkipGramSequence
 from .link_prediction_sequence import LinkPredictionSequence
--- embiggen/transformers/__init__.py
+++ embiggen/transformers/__init__.py
@@ -1,3 +1,4 @@
+"""Module with method to embed graphs using a precomputed embedding."""
 from .edge_transformer import EdgeTransformer
 from .node_transformer import NodeTransformer
 from .graph_transformer import GraphTransformer
