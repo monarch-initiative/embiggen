51c0eaa73e3a826d22151e79f68b6da4d647cbfc
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-07-17 09:52:21 +0200

    Added new sequences to init

--- embiggen/__init__.py
+++ embiggen/__init__.py
@@ -1,9 +1,10 @@
 """Module with models to compute graph embedding and relative Keras Sequences to train them."""
 from .embedders import CBOW, SkipGram, GloVe, BinarySkipGram
 from .transformers import NodeTransformer, EdgeTransformer, GraphTransformer
-from .sequences import NodeBinarySkipGramSequence, Node2VecSequence, LinkPredictionSequence
+from .sequences import NodeBinarySkipGramSequence, Node2VecSequence, LinkPredictionSequence, WordBinarySkipGramSequence, Word2VecSequence
 
 __all__ = [
     "CBOW", "SkipGram", "GloVe", "BinarySkipGram",
-    "NodeBinarySkipGramSequence", "LinkPredictionSequence", "Node2VecSequence"
+    "NodeBinarySkipGramSequence", "LinkPredictionSequence", "Node2VecSequence",
+    "WordBinarySkipGramSequence", "Word2VecSequence"
 ]
