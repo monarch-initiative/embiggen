35b414aea4fcaf39dca3a7285a4534f19cffba0b
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-07-22 15:47:32 +0200

    Resolved code smells

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,8 +1,12 @@
 """Test to validate that version code is written in PEP8 compliant format."""
+from unittest import TestCase
 from validate_version_code import validate_version_code
 from embiggen.__version__ import __version__
 
 
-def test_version():
-    """Test to validate that version code is written in PEP8 compliant format."""
-    assert validate_version_code(__version__)
+class TestTextTransformerSentences(TestCase):
+    """Unit test to validate that version code is compliant to PEP8 format."""
+
+    def test_version(self):
+        """Test to validate version code."""
+        self.assertTrue(validate_version_code(__version__))
