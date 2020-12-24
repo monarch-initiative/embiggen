536eb264debdef9a5bafc3179e96a8dbcce6a555
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-12-24 17:13:10 +0100

    Updated version

--- embiggen/__version__.py
+++ embiggen/__version__.py
@@ -1,2 +1,2 @@
 """Current version of package embiggen"""
-__version__ = "0.6.1"
+__version__ = "0.8.0"
--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ authors = {
 
 setup(
     name='embiggen',
-    version='0.7.0',
+    version='0.8.0',
     description='Extended implementation of node2vec with several word2vec family algorithms',
     long_description=readme(),
     url='https://github.com/monarch-initiative/embiggen',
