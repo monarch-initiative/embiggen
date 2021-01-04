baa6da7971a5ddd942fa1753b2abac5ecb41fcd6
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-01-04 16:19:37 +0100

    Created new FFNN model

--- embiggen/link_prediction/__init__.py
+++ embiggen/link_prediction/__init__.py
@@ -1,7 +1,9 @@
 from .perceptron import Perceptron
 from .multi_layer_perceptron import MultiLayerPerceptron
+from .ffnn import FFNN
 
 __all__ = [
     "Perceptron",
-    "MultiLayerPerceptron"
+    "MultiLayerPerceptron",
+    "FFNN"
 ]
