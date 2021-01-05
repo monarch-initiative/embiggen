f8ec244a22d8ad4d93efac489461db85839219da
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-01-05 14:54:51 +0100

    Added l1l2 regularization to perceptron model

--- embiggen/link_prediction/perceptron.py
+++ embiggen/link_prediction/perceptron.py
@@ -1,5 +1,7 @@
 """Class for creating a Perceptron model for link prediction tasks."""
+from tensorflow.keras import regularizers
 from tensorflow.keras.layers import Dense, Layer
+
 from .link_prediction_model import LinkPredictionModel
 
 
@@ -7,4 +9,8 @@ class Perceptron(LinkPredictionModel):
 
     def _build_model_body(self, input_layer: Layer) -> Layer:
         """Build new model body for link prediction."""
-        return Dense(1, activation="sigmoid")(input_layer)
+        return Dense(
+            units=1,
+            activation="sigmoid",
+            kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+        )(input_layer)
