5175e152112c5c6a12e6a7b4d4974d96ca8e609c
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-01-07 12:27:02 +0100

    Added back MLP model

--- embiggen/link_prediction/__init__.py
+++ embiggen/link_prediction/__init__.py
@@ -1,6 +1,8 @@
 """Module implementing link prediction models."""
 from .perceptron import Perceptron
+from .multi_layer_perceptron import MultiLayerPerceptron
 
 __all__ = [
     "Perceptron",
+    "MultiLayerPerceptron"
 ]
---
+++ embiggen/link_prediction/multi_layer_perceptron.py
@@ -0,0 +1,26 @@
+"""Class for creating a MultiLayerPerceptron model for link prediction tasks."""
+from tensorflow.keras import regularizers
+from tensorflow.keras.layers import Dense, Layer
+
+from .link_prediction_model import LinkPredictionModel
+
+
+class MultiLayerPerceptron(LinkPredictionModel):
+
+    def _build_model_body(self, input_layer: Layer) -> Layer:
+        """Build new model body for link prediction."""
+        input_layer = Dense(
+            units=32,
+            activation="relu",
+            kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+        )(input_layer)
+        input_layer = Dense(
+            units=32,
+            activation="relu",
+            kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+        )(input_layer)
+        return Dense(
+            units=1,
+            activation="sigmoid",
+            kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4),
+        )(input_layer)
