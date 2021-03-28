f1cc44471fb6feec75bbe79f8da4e9586fc1d695
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-03-28 22:57:00 +0200

    Updated requirements

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         "keras_mixed_sequence>=1.0.20",
         "extra_keras_metrics>=2.0.0",
         "ensmallen_graph>=0.5.2",
-        "cache_decorator>=2.0.0"
+        "cache_decorator>=2.0.1"
     ],
     tests_require=test_deps,
     include_package_data=True,
