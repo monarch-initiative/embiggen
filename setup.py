848f9388b11fe8ab42da09a89b4c0e1e7e136555
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-04-06 18:50:08 +0200

    Updated version of embiggen

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
         'tensorflow>=2.0.1',
         "keras_mixed_sequence>=1.0.20",
         "extra_keras_metrics>=2.0.0",
-        "ensmallen_graph>=0.5.2",
+        "ensmallen_graph>=0.6.0",
         "cache_decorator>=2.0.1"
     ],
     tests_require=test_deps,
