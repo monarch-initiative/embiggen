edb875eed4b2290d6ab909ab9bffd10f9eb0dda6
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-04-08 11:43:35 +0200

    Updated requirement for sanitize ml labels

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
         "matplotlib",
         "sklearn",
         "ddd_subplots>=1.0.7",
-        "sanitize_ml_labels>=1.0.12",
+        "sanitize_ml_labels>=1.0.25",
         'tensorflow>=2.0.1',
         "keras_mixed_sequence>=1.0.20",
         "extra_keras_metrics>=2.0.0",
