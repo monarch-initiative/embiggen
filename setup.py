ba8e65d35ff950820919d2948739f1da78e9b0b5
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-09-06 17:13:36 +0200

    Published new embiggen version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ authors = {
 
 setup(
     name='embiggen',
-    version='0.8.5',
+    version='0.9.0',
     description='Extended implementation of node2vec with several word2vec family algorithms',
     long_description=readme(),
     url='https://github.com/monarch-initiative/embiggen',
