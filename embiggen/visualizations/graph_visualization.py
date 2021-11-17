08db2bff2483a1de58456cf4719caf373d124160
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-11-17 19:15:42 +0100

    Added support for fit TSNE

--- embiggen/visualizations/graph_visualization.py
+++ embiggen/visualizations/graph_visualization.py
@@ -150,49 +150,59 @@ class GraphVisualization:
                 })
             except (ModuleNotFoundError, NotImplementedError):
                 try:
-                    from MulticoreTSNE import \
-                        MulticoreTSNE  # pylint: disable=import-outside-toplevel
-                    return MulticoreTSNE(**{
-                        **dict(
-                            n_components=self._n_components,
-                            n_jobs=cpu_count(),
-                            random_state=self._random_state,
-                            verbose=True,
-                        ),
-                        **self._decomposition_kwargs
-                    })
-                except (ModuleNotFoundError, OSError, RuntimeError):
+                    from fitsne import FItSNE
+                    return lambda X: FItSNE(
+                        X=X,
+                        no_dims=4,
+                        nthreads=cpu_count(),
+                        rand_seed=self._random_state,
+                        max_iter=1000,
+                    )
+                except:
                     try:
-                        from sklearn.manifold import \
-                            TSNE  # pylint: disable=import-outside-toplevel
-                        return TSNE(**{
+                        from MulticoreTSNE import \
+                            MulticoreTSNE  # pylint: disable=import-outside-toplevel
+                        return MulticoreTSNE(**{
                             **dict(
                                 n_components=self._n_components,
                                 n_jobs=cpu_count(),
                                 random_state=self._random_state,
                                 verbose=True,
-                                method="exact" if self._n_components == 4 else "barnes_hut",
-                                square_distances=True,
                             ),
                             **self._decomposition_kwargs
                         })
-                    except:
-                        raise ModuleNotFoundError(
-                            "You do not have installed a supported TSNE "
-                            "decomposition algorithm. Depending on your use case, "
-                            "we suggest you install tsne-cuda if your graph is "
-                            "very big (in the millions of nodes) if you have access "
-                            "to a compatible GPU system.\n"
-                            "Alternatively, we suggest (and support) MulticoreTSNE, "
-                            "which tends to be easier to install, and is significantly "
-                            "faster than the Sklearn implementation.\n"
-                            "Alternatively, we suggest (and support) MulticoreTSNE, "
-                            "which tends to be easier to install, and is significantly "
-                            "faster than the Sklearn implementation.\n"
-                            "If you intend to do 3D decompositions, "
-                            "remember that tsne-cuda, at the time of writing, "
-                            "does not support them."
-                        )
+                    except (ModuleNotFoundError, OSError, RuntimeError):
+                        try:
+                            from sklearn.manifold import \
+                                TSNE  # pylint: disable=import-outside-toplevel
+                            return TSNE(**{
+                                **dict(
+                                    n_components=self._n_components,
+                                    n_jobs=cpu_count(),
+                                    random_state=self._random_state,
+                                    verbose=True,
+                                    method="exact" if self._n_components == 4 else "barnes_hut",
+                                    square_distances=True,
+                                ),
+                                **self._decomposition_kwargs
+                            })
+                        except:
+                            raise ModuleNotFoundError(
+                                "You do not have installed a supported TSNE "
+                                "decomposition algorithm. Depending on your use case, "
+                                "we suggest you install tsne-cuda if your graph is "
+                                "very big (in the millions of nodes) if you have access "
+                                "to a compatible GPU system.\n"
+                                "Alternatively, we suggest (and support) MulticoreTSNE, "
+                                "which tends to be easier to install, and is significantly "
+                                "faster than the Sklearn implementation.\n"
+                                "Alternatively, we suggest (and support) MulticoreTSNE, "
+                                "which tends to be easier to install, and is significantly "
+                                "faster than the Sklearn implementation.\n"
+                                "If you intend to do 3D decompositions, "
+                                "remember that tsne-cuda, at the time of writing, "
+                                "does not support them."
+                            )
         elif self._decomposition_method == "PCA":
             return PCA(**{
                 **dict(
