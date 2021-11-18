1a6c2e671949e2e174186081e87a919c84859d91
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-11-18 19:28:56 +0100

    Updated requirements

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
         "tqdm",
         "matplotlib",
         "sklearn",
-        "ddd_subplots>=1.0.15",
+        "ddd_subplots>=1.0.16",
         "sanitize_ml_labels>=1.0.27",
         "keras_mixed_sequence>=1.0.27",
         "extra_keras_metrics>=2.0.3",
