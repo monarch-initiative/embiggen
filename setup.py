b7697d632d9963ef21bf3813215cc6fb57f30ffc
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-12-13 12:09:18 +0100

    Bumped requirements

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
         "sklearn",
         "ddd_subplots>=1.0.17",
         "sanitize_ml_labels>=1.0.29",
-        "keras_mixed_sequence>=1.0.27",
+        "keras_mixed_sequence>=1.0.28",
         "extra_keras_metrics>=2.0.5",
         "ensmallen>=0.6.1",
         "validate_version_code",
