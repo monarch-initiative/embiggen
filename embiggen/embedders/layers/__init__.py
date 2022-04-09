6967b8727212ba402cf978cab55bc20b653fc9a5
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-04-09 08:35:57 +0200

    Removed deprecated code

--- embiggen/embedders/layers/__init__.py
+++ embiggen/embedders/layers/__init__.py
@@ -2,11 +2,9 @@
 from .noise_contrastive_estimation import NoiseContrastiveEstimation
 from .sampled_softmax import SampledSoftmax
 from .graph_convolution_layer import GraphConvolution
-from .graph_attention import GraphAttention
 
 __all__ = [
     "NoiseContrastiveEstimation",
     "GraphConvolution",
     "SampledSoftmax",
-    "GraphAttention"
 ]
