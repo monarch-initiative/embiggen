3413789b6f81c8ee3fb41de8ac791f2baa707223
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-04-14 15:12:37 +0200

    Addded tensorflow wrapper

--- embiggen/__init__.py
+++ embiggen/__init__.py
@@ -1,10 +1,17 @@
 """Module with models for graph and text embedding and their Keras Sequences."""
-from .embedders import (
-    CBOW, GloVe, GraphCBOW, GraphGloVe, GraphSkipGram,
-    SkipGram, TransE, TransH, TransR, SimplE, Siamese
-)
-from .node_prediction import GraphConvolutionalNeuralNetwork
-from .sequences import Word2VecSequence
+import warnings
+try:
+    from .embedders import (
+        CBOW, GloVe, GraphCBOW, GraphGloVe, GraphSkipGram,
+        SkipGram, TransE, TransH, TransR, SimplE, Siamese
+    )
+    from .node_prediction import GraphConvolutionalNeuralNetwork
+    from .sequences import Word2VecSequence
+except ModuleNotFoundError:
+    warnings.warn(
+        "You do not have TensorFlow installed, all tensorflow models "
+        "will not work."
+    )
 from .transformers import (CorpusTransformer, EdgeTransformer,
                            GraphTransformer, LinkPredictionTransformer,
                            NodeTransformer)
