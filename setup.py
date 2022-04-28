e311e932392b3361fb0e377bacef4287b06ef4ea
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-04-28 19:11:11 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
         "sanitize_ml_labels>=1.0.31",
         "keras_mixed_sequence>=1.0.28",
         "extra_keras_metrics>=2.0.7",
-        "ensmallen>=0.7.0.dev15",
+        "ensmallen>=0.7.0.dev16",
         "validate_version_code",
         "packaging"
     ],
