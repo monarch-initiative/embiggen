1c815856f8fba5021de49b2e8716128a014cecaf
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-05-04 00:02:59 +0200

    Handled corner case in figure caption

--- embiggen/visualizations/graph_visualizer.py
+++ embiggen/visualizations/graph_visualizer.py
@@ -58,7 +58,12 @@ def format_list(
     bold_words: bool = False
         Whether to use bold letters.
     """
-    return ", ".join([
+    if len(words) == 2:
+        joiner = " "
+    else:
+        joiner = ", "
+
+    return joiner.join([
         "{optional_and}{open_bold}{word}{close_bold}".format(
             word=word,
             optional_and="and " if i > 0 and i == len(words) - 1 else "",
