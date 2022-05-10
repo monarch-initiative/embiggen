d0c153d53d8252527970f311c22ee8081e7fef73
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-05-10 11:42:15 +0200

    Added more sklearn models wrappers

---
+++ embiggen/edge_prediction/edge_prediction_sklearn/extra_trees_edge_prediction.py
@@ -0,0 +1,109 @@
+"""Submodule wrapping Extra Trees for edge prediction."""
+from typing import Dict, Any
+from sklearn.ensemble import ExtraTreesClassifier
+from .sklearn_edge_prediction_adapter import SklearnEdgePredictionAdapter
+
+
+class ExtraTreesEdgePrediction(SklearnEdgePredictionAdapter):
+    """Create wrapper over Sklearn Extra Trees classifier for edge prediction."""
+
+    def __init__(
+        self,
+        n_estimators: int = 1000,
+        criterion: str = "gini",
+        max_depth: int = 10,
+        min_samples_split: int = 2,
+        min_samples_leaf: int = 1,
+        min_weight_fraction_leaf: float = 0.,
+        max_features="auto",
+        max_leaf_nodes=None,
+        min_impurity_decrease=0.,
+        min_impurity_split=None,
+        bootstrap=True,
+        oob_score=False,
+        n_jobs=-1,
+        verbose=0,
+        warm_start=False,
+        class_weight=None,
+        ccp_alpha=0.0,
+        max_samples=None,
+        edge_embedding_method: str = "Concatenate",
+        unbalance_rate: float = 1.0,
+        sample_only_edges_with_heterogeneous_node_types: bool = False,
+        random_state: int = 42
+    ):
+        """Create the Extra Trees for Edge  Prediction."""
+        self._n_estimators = n_estimators
+        self._criterion = criterion
+        self._max_depth = max_depth
+        self._min_samples_split = min_samples_split
+        self._min_samples_leaf = min_samples_leaf
+        self._min_weight_fraction_leaf = min_weight_fraction_leaf
+        self._max_features = max_features
+        self._max_leaf_nodes = max_leaf_nodes
+        self._min_impurity_decrease = min_impurity_decrease
+        self._min_impurity_split = min_impurity_split
+        self._bootstrap = bootstrap
+        self._oob_score = oob_score
+        self._n_jobs = n_jobs
+        self._random_state = random_state
+        self._verbose = verbose
+        self._warm_start = warm_start
+        self._class_weight = class_weight
+        self._ccp_alpha = ccp_alpha
+        self._max_samples = max_samples
+
+        super().__init__(
+            ExtraTreesClassifier(
+                n_estimators=n_estimators,
+                criterion=criterion,
+                max_depth=max_depth,
+                min_samples_split=min_samples_split,
+                min_samples_leaf=min_samples_leaf,
+                min_weight_fraction_leaf=min_weight_fraction_leaf,
+                max_features=max_features,
+                max_leaf_nodes=max_leaf_nodes,
+                min_impurity_decrease=min_impurity_decrease,
+                min_impurity_split=min_impurity_split,
+                bootstrap=bootstrap,
+                oob_score=oob_score,
+                n_jobs=n_jobs,
+                random_state=random_state,
+                verbose=verbose,
+                warm_start=warm_start,
+                class_weight=class_weight,
+                ccp_alpha=ccp_alpha,
+                max_samples=max_samples
+            ),
+            edge_embedding_method,
+            unbalance_rate,
+            sample_only_edges_with_heterogeneous_node_types,
+            random_state
+        )
+
+    def parameters(self) -> Dict[str, Any]:
+        """Returns parameters used for this model."""
+        return {
+            **super().parameters(),
+            **dict(
+                n_estimators = self._n_estimators,
+                criterion = self._criterion,
+                max_depth = self._max_depth,
+                min_samples_split = self._min_samples_split,
+                min_samples_leaf = self._min_samples_leaf,
+                min_weight_fraction_leaf = self._min_weight_fraction_leaf,
+                max_features = self._max_features,
+                max_leaf_nodes = self._max_leaf_nodes,
+                min_impurity_decrease = self._min_impurity_decrease,
+                min_impurity_split = self._min_impurity_split,
+                bootstrap = self._bootstrap,
+                oob_score = self._oob_score,
+                n_jobs = self._n_jobs,
+                random_state = self._random_state,
+                verbose = self._verbose,
+                warm_start = self._warm_start,
+                class_weight = self._class_weight,
+                ccp_alpha = self._ccp_alpha,
+                max_samples = self._max_samples,
+            )
+        }
---
+++ embiggen/edge_prediction/edge_prediction_sklearn/gradient_boosting_edge_prediction.py
@@ -0,0 +1,103 @@
+"""Submodule wrapping Gradient Boosting for edge prediction."""
+from typing import Dict, Any
+from sklearn.ensemble import GradientBoostingClassifier
+from .sklearn_edge_prediction_adapter import SklearnEdgePredictionAdapter
+
+
+class GradientBoostingEdgePrediction(SklearnEdgePredictionAdapter):
+    """Create wrapper over Sklearn Gradient Boosting classifier for edge prediction."""
+
+    def __init__(
+        self,
+        loss='deviance',
+        learning_rate=0.1,
+        n_estimators=100,
+        subsample=1.0,
+        criterion='friedman_mse',
+        min_samples_split=2,
+        min_samples_leaf=1,
+        min_weight_fraction_leaf=0.,
+        max_depth=3,
+        min_impurity_decrease=0.,
+        min_impurity_split=None,
+        init=None,
+        max_features=None,
+        verbose=0,
+        max_leaf_nodes=None,
+        warm_start=False,
+        validation_fraction=0.1,
+        n_iter_no_change=None,
+        tol=1e-4,
+        ccp_alpha=0.0,
+        edge_embedding_method: str = "Concatenate",
+        unbalance_rate: float = 1.0,
+        sample_only_edges_with_heterogeneous_node_types: bool = False,
+        random_state: int = 42
+    ):
+        """Create the Gradient Boosting for Edge Prediction."""
+        self._loss=loss
+        self._learning_rate=learning_rate
+        self._n_estimators=n_estimators,
+        self._criterion=criterion
+        self._min_samples_split=min_samples_split,
+        self._min_samples_leaf=min_samples_leaf,
+        self._min_weight_fraction_leaf=min_weight_fraction_leaf,
+        self._max_depth=max_depth
+        self._init=init
+        self._subsample=subsample,
+        self._max_features=max_features,
+        self._max_leaf_nodes=max_leaf_nodes,
+        self._min_impurity_decrease=min_impurity_decrease,
+        self._min_impurity_split=min_impurity_split,
+        self._warm_start=warm_start
+        self._validation_fraction=validation_fraction,
+        self._n_iter_no_change=n_iter_no_change
+        self._tol=tol
+        self._ccp_alpha=ccp_alpha
+        super().__init__(
+            GradientBoostingClassifier(
+                loss=loss, learning_rate=learning_rate, n_estimators=n_estimators,
+                criterion=criterion, min_samples_split=min_samples_split,
+                min_samples_leaf=min_samples_leaf,
+                min_weight_fraction_leaf=min_weight_fraction_leaf,
+                max_depth=max_depth, init=init, subsample=subsample,
+                max_features=max_features,
+                random_state=random_state, verbose=verbose,
+                max_leaf_nodes=max_leaf_nodes,
+                min_impurity_decrease=min_impurity_decrease,
+                min_impurity_split=min_impurity_split,
+                warm_start=warm_start, validation_fraction=validation_fraction,
+                n_iter_no_change=n_iter_no_change, tol=tol, ccp_alpha=ccp_alpha
+            ),
+            edge_embedding_method,
+            unbalance_rate,
+            sample_only_edges_with_heterogeneous_node_types,
+            random_state
+        )
+
+    def parameters(self) -> Dict[str, Any]:
+        """Returns parameters used for this model."""
+        return {
+            **super().parameters(),
+            **dict(
+                loss=self._loss,
+                learning_rate=self._learning_rate,
+                n_estimators=self._n_estimators,
+                criterion=self._criterion,
+                min_samples_split=self._min_samples_split,
+                min_samples_leaf=self._min_samples_leaf,
+                min_weight_fraction_leaf=self._min_weight_fraction_leaf,
+                max_depth=self._max_depth,
+                init=self._init,
+                subsample=self._subsample,
+                max_features=self._max_features,
+                max_leaf_nodes=self._max_leaf_nodes,
+                min_impurity_decrease=self._min_impurity_decrease,
+                min_impurity_split=self._min_impurity_split,
+                warm_start=self._warm_start,
+                validation_fraction=self._validation_fraction,
+                n_iter_no_change=self._n_iter_no_change,
+                tol=self._tol,
+                ccp_alpha=self._ccp_alpha
+            )
+        }
