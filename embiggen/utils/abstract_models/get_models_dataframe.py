98b566af9a515d082c45272c401065a31c54d5c6
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-05-13 15:40:55 +0200

    Fixed typos

--- embiggen/utils/abstract_models/get_models_dataframe.py
+++ embiggen/utils/abstract_models/get_models_dataframe.py
@@ -21,16 +21,16 @@ def get_models_dataframe() -> pd.DataFrame:
 def get_available_models_for_edge_prediction() -> pd.DataFrame:
     """Returns dataframe with informations about available models for edge prediction."""
     df = get_models_dataframe()
-    return df[(df.task_name == "Edge prediction") & df.available]
+    return df[(df.task_name == "Edge Prediction") & df.available]
 
 
 def get_available_models_for_edge_label_prediction() -> pd.DataFrame:
     """Returns dataframe with informations about available models for edge-label prediction."""
     df = get_models_dataframe()
-    return df[(df.task_name == "Edge Label prediction") & df.available]
+    return df[(df.task_name == "Edge Label Prediction") & df.available]
 
 
 def get_available_models_for_node_label_prediction() -> pd.DataFrame:
     """Returns dataframe with informations about available models for node-label prediction."""
     df = get_models_dataframe()
-    return df[(df.task_name == "Node Label prediction") & df.available]
+    return df[(df.task_name == "Node Label Prediction") & df.available]
