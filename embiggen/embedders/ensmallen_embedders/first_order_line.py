7afe8756f8e50d4793dab0221f4044424103b91e
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-06-14 18:01:54 +0200

    Working on LINE

--- embiggen/embedders/ensmallen_embedders/first_order_line.py
+++ embiggen/embedders/ensmallen_embedders/first_order_line.py
@@ -1,64 +1,59 @@
-"""Module providing abstract Node2Vec implementation."""
-from typing import Optional, Union, Dict, Any
+"""Module providing FirstOrderLINE model implementation."""
+from typing import Optional, Dict, Any, Union
 from ensmallen import Graph
+from ensmallen import models
 import numpy as np
 import pandas as pd
-from ensmallen import models
-from embiggen.utils.abstract_models import abstract_class, AbstractEmbeddingModel, EmbeddingResult
+from embiggen.utils.abstract_models import AbstractEmbeddingModel, EmbeddingResult
 
 
-@abstract_class
-class Node2VecEnsmallen(AbstractEmbeddingModel):
-    """Abstract class for Node2Vec algorithms."""
-
-    MODELS = {
-        "cbow": models.CBOW,
-        "skipgram": models.SkipGram
-    }
+class FirstOrderLINEEnsmallen(AbstractEmbeddingModel):
+    """Class providing FirstOrderLINE implemeted in Rust from Ensmallen."""
 
     def __init__(
         self,
-        model_name: str,
+        embedding_size: int = 100,
+        negative_samples_rate: float = 0.9,
         epochs: int = 10,
         learning_rate: float = 0.01,
         learning_rate_decay: float = 0.9,
-        enable_cache: bool = False,
-        **kwargs
+        random_state: int = 42,
+        enable_cache: bool = False
     ):
         """Create new abstract Node2Vec method.
 
         Parameters
         --------------------------
-        model_name: str
-            The ensmallen graph embedding model to use.
-            Can either be the SkipGram of CBOW models.
+        embedding_size: int = 100
+            Dimension of the embedding.
+        negative_samples_rate: float = 0.9
+            Rate of negative samples per rate.
         epochs: int = 10
             Number of epochs to train the model for.
         learning_rate: float = 0.01
-            The learning rate to use to train the Node2Vec model.
+            The learning rate to use to train the Node2Vec model. By default 0.01.
         learning_rate_decay: float = 0.9
             Factor to reduce the learning rate for at each epoch. By default 0.9.
+        random_state: int = 42
+            The random state to reproduce the training sequence.
         enable_cache: bool = False
             Whether to enable the cache, that is to
             store the computed embedding.
         """
-        if model_name.lower() not in self.MODELS:
-            raise ValueError(
-                (
-                    "The provided model name {} is not supported. "
-                    "The supported models are `CBOW` and `SkipGram`."
-                ).format(model_name)
-            )
-
         self._epochs = epochs
         self._learning_rate = learning_rate
         self._learning_rate_decay = learning_rate_decay
-        self._model_kwargs = kwargs
 
-        self._model = Node2VecEnsmallen.MODELS[model_name.lower()](**kwargs)
+        self._model_kwargs = dict(
+            embedding_size=embedding_size,
+            negative_samples_rate=negative_samples_rate,
+            random_state=random_state,
+        )
+
+        self._model = models.FirstOrderLINE(**self._model_kwargs)
 
         super().__init__(
-            embedding_size=kwargs["embedding_size"],
+            embedding_size=embedding_size,
             enable_cache=enable_cache
         )
 
@@ -68,11 +63,6 @@ class Node2VecEnsmallen(AbstractEmbeddingModel):
         return dict(
             embedding_size=5,
             epochs=1,
-            window_size=1,
-            walk_length=4,
-            iterations=1,
-            max_neighbours=10,
-            number_of_negative_samples=1
         )
 
     def parameters(self) -> Dict[str, Any]:
@@ -131,31 +121,44 @@ class Node2VecEnsmallen(AbstractEmbeddingModel):
 
     @staticmethod
     def requires_positive_edge_weights() -> bool:
-        return True
+        return False
+
+    @staticmethod
+    def model_name() -> str:
+        """Returns name of the model."""
+        return "First Order LINE"
+
+    @staticmethod
+    def requires_node_types() -> bool:
+        return False
+
+    @staticmethod
+    def requires_edge_types() -> bool:
+        return False
 
     @staticmethod
     def can_use_edge_weights() -> bool:
         """Returns whether the model can optionally use edge weights."""
-        return True
+        return False
 
     def is_using_edge_weights(self) -> bool:
         """Returns whether the model is parametrized to use edge weights."""
-        return True
+        return False
 
     @staticmethod
     def can_use_node_types() -> bool:
         """Returns whether the model can optionally use node types."""
-        return True
+        return False
 
     def is_using_node_types(self) -> bool:
         """Returns whether the model is parametrized to use node types."""
-        return self._model_kwargs["change_node_type_weight"] != 1.0
+        return False
 
     @staticmethod
     def can_use_edge_types() -> bool:
         """Returns whether the model can optionally use edge types."""
-        return True
+        return False
 
     def is_using_edge_types(self) -> bool:
         """Returns whether the model is parametrized to use edge types."""
-        return self._model_kwargs["change_edge_type_weight"] != 1.0
+        return False
--- embiggen/embedders/ensmallen_embedders/spine.py
+++ embiggen/embedders/ensmallen_embedders/spine.py
@@ -56,7 +56,7 @@ class SPINE(AbstractEmbeddingModel):
         graph: Graph,
         return_dataframe: bool = True,
         verbose: bool = True
-    ) -> Union[np.ndarray, pd.DataFrame]:
+    ) -> EmbeddingResult:
         """Return node embedding."""
         node_embedding = self._model.fit_transform(
             graph,
