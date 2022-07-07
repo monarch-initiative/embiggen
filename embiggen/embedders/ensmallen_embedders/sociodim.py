c205f31e45415e7809371125b1b66032170e5d5c
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-07-07 09:40:56 +0200

    Added support for sparse and dense eigenvalues computation

--- embiggen/embedders/ensmallen_embedders/sociodim.py
+++ embiggen/embedders/ensmallen_embedders/sociodim.py
@@ -4,6 +4,7 @@ from ensmallen import Graph
 import pandas as pd
 import numpy as np
 from scipy.linalg import eigh
+from scipy.sparse.linalg import eigsh
 from embiggen.utils.abstract_models import AbstractEmbeddingModel, EmbeddingResult
 
 
@@ -13,6 +14,7 @@ class SocioDimEnsmallen(AbstractEmbeddingModel):
     def __init__(
         self,
         embedding_size: int = 100,
+        use_sparse_reduce: bool = True,
         enable_cache: bool = False
     ):
         """Create new SocioDim method.
@@ -21,14 +23,30 @@ class SocioDimEnsmallen(AbstractEmbeddingModel):
         --------------------------
         embedding_size: int = 100
             Dimension of the embedding.
+        use_sparse_reduce: bool = True
+            Whether to use the sparse reduce or the dense reduce for
+            the computation of eigenvectors.
+            For both reduce mechanisms, we are using LAPACK implementations.
+            For some currently unknown reason, their implementation using
+            a sparse reduce, even on dense matrices, yields better results.
         enable_cache: bool = False
             Whether to enable the cache, that is to
             store the computed embedding.
         """
+        self._use_sparse_reduce = use_sparse_reduce
         super().__init__(
             embedding_size=embedding_size,
             enable_cache=enable_cache,
         )
+
+    def parameters(self) -> Dict[str, Any]:
+        """Returns parameters of the model."""
+        return {
+            **super().parameters(),
+            **dict(
+                use_sparse_reduce=self._use_sparse_reduce,
+            )
+        }
 
     def _fit_transform(
         self,
@@ -37,14 +55,22 @@ class SocioDimEnsmallen(AbstractEmbeddingModel):
         verbose: bool = True
     ) -> EmbeddingResult:
         """Return node embedding."""
-        number_of_nodes = graph.get_number_of_nodes()
-        embedding = eigh(
-            graph.get_dense_modularity_matrix(),
-            eigvals=(
-                number_of_nodes-self._embedding_size,
-                number_of_nodes-1
-            )
-        )[1]
+        if self._use_sparse_reduce:
+            embedding = eigsh(
+                graph.get_dense_modularity_matrix(),
+                k=self._embedding_size,
+                which="LM",
+                return_eigenvectors=True
+            )[1]
+        else:
+            number_of_nodes = graph.get_number_of_nodes()
+            embedding = eigh(
+                graph.get_dense_modularity_matrix(),
+                eigvals=(
+                    number_of_nodes-self._embedding_size,
+                    number_of_nodes-1
+                )
+            )[1]
 
         if return_dataframe:
             node_names = graph.get_node_names()
--- embiggen/embedders/karateclub_embedders/sociodim.py
+++ embiggen/embedders/karateclub_embedders/sociodim.py
@@ -1,5 +1,4 @@
 """Wrapper for SocioDim model provided from the Karate Club package."""
-from typing import Dict, Any
 from karateclub.node_embedding import SocioDim
 from embiggen.embedders.karateclub_embedders.abstract_karateclub_embedder import AbstractKarateClubEmbedder
 
