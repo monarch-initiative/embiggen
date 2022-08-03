bd620c718bc3febe9fa5b6c37f6e26efac823e2f
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-08-03 10:55:27 +0200

    Debugging histograms

--- embiggen/visualizations/graph_visualizer.py
+++ embiggen/visualizations/graph_visualizer.py
@@ -2273,6 +2273,7 @@ class GraphVisualizer:
             log=True,
             label=["Non-existent", "Existent"]
         )
+        axes.set_xlim(edge_metrics.min(), edge_metrics.max())
         print(metric_name, edge_metrics.min(), edge_metrics.max())
         axes.set_ylabel("Counts (log scale)")
         axes.set_xlabel(metric_name)
