3fc86b220def08b861025c3409194230cb2eba13
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-08-04 17:11:38 +0200

    Update __init__.py
    References #285

--- embiggen/embedders/pykeen_embedders/__init__.py
+++ embiggen/embedders/pykeen_embedders/__init__.py
@@ -1,8 +1,8 @@
-"""Module with graph embedding models based on PyKeen."""
+"""Module with graph embedding models based on PyKEEN."""
 from embiggen.utils.abstract_models import build_init, AbstractEmbeddingModel
 
 build_init(
     module_library_names=["torch", "pykeen"],
-    formatted_library_name="PyKeen",
+    formatted_library_name="PyKEEN",
     expected_parent_class=AbstractEmbeddingModel
 )
