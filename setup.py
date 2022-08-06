077c0a618cf47881010b0aa1dc600bfb2aaa3856
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-08-06 15:50:07 +0200

    Updated requirements and version

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
         "ddd_subplots>=1.0.20",
         "sanitize_ml_labels>=1.0.38",
         "keras_mixed_sequence>=1.0.28",
-        "ensmallen>=0.8.8",
+        "ensmallen>=0.8.11",
         "compress_pickle>=2.1.0",
         "validate_version_code",
         "cache_decorator>=2.1.9",
