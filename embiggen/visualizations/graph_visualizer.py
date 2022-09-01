06e370ff5793a9837166977cce30c59da07fdf3b
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-09-01 22:16:18 +0200

    Fixing visualization error

--- embiggen/visualizations/graph_visualizer.py
+++ embiggen/visualizations/graph_visualizer.py
@@ -4385,12 +4385,12 @@ class GraphVisualizer:
 
             if show_letters:
                 if self._n_components >= 3:
-                    additional_kwargs = dict(z=-0.1)
+                    additional_kwargs = dict(z=1)
                 else:
                     additional_kwargs = dict()
 
                 ax.text(
-                    x=-0.1,
+                    x=0,
                     y=1.1,
                     s=letter,
                     size=18,
