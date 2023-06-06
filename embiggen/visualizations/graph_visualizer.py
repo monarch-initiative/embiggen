59ce6cade7d9044452bcc63ab0a9f5a2a12c2690
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2023-06-06 08:46:53 +0200

    Re-added node-degree distribution plotting for graph visualizer in edge embedding

--- embiggen/visualizations/graph_visualizer.py
+++ embiggen/visualizations/graph_visualizer.py
@@ -4578,9 +4578,10 @@ class GraphVisualizer:
             node_scatter_plot_methods_to_call.append(
                 self.plot_node_degrees,
             )
-            distribution_plot_methods_to_call.append(
-                self.plot_node_degree_distribution,
-            )
+        
+        distribution_plot_methods_to_call.append(
+            self.plot_node_degree_distribution,
+        )
 
         def plot_distance_wrapper(plot_distance):
             @functools.wraps(plot_distance)
