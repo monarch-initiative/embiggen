54aecbc8a923f4b5e6be40b6c24fa3cbadf22430
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2023-06-10 21:58:55 +0200

    Removed temporary files

--- test_mctsne.py
+++
@@ -1,6 +0,0 @@
-from sklearn.datasets import load_digits
-from MulticoreTSNE import MulticoreTSNE as TSNE
-from matplotlib import pyplot as plt
-
-digits = load_digits()
-embeddings = TSNE(n_jobs=4).fit_transform(digits.data)
